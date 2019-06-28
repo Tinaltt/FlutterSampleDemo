91837dd8ce6631e17b04f47fc5f225fb5af69b6a
Tina <11289790+Tinaltt@users.noreply.github.com>
2019-06-28 10:26:08 +0800

    test

--- android/app/src/main/java/com/example/flutter_demo_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_demo_app/MainActivity.java
@@ -12,7 +12,6 @@ import android.util.Log;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.EventChannel;
-import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
